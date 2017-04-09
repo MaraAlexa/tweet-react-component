5276d9f6de81e9d1bcced649fa60aa46377e306c
maraalexa <maraalexa57@gmail.com>
2017-04-09 13:11:32 +0200

    added glamor for css styling

--- src/components/envelope/Envelope.js
+++ src/components/envelope/Envelope.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import AddressLabel from '../address/AddressLabel';
 import Stamp from './Stamp';
+import { css } from 'glamor';
+
+let receiverStyle = css({
+  color: 'blue',
+  marginLeft: '300px'
+})
+
+let envelopeStyle = css({
+  marginTop: '3rem',
+  border: '1px solid black',
+  padding: '20px'
+})
+
 
 class Envelope extends React.Component {
   render() {
     const {toPerson, fromPerson} = this.props;
     return(
-      <div className="envelope">
+      <div className={`${envelopeStyle}`}>
         <AddressLabel className='from-label' mailingLabel={fromPerson} />
-        <AddressLabel className='to-label' mailingLabel={toPerson} />
+        <AddressLabel className={`to-label ${receiverStyle}`} mailingLabel={toPerson} />
         <Stamp />
       </div>
     )
--- src/components/envelope/Stamp.js
+++ src/components/envelope/Stamp.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { css } from 'glamor';
+
+let stampStyle = css({
+  position: 'absolute',
+  top: 240,
+  right: 100,
+  border: "1px solid black",
+  padding: 20,
+  width: 80,
+  textAlign: "center"
+})
+
 
 class Stamp extends React.Component {
   render(){
     return(
-      <div className="stamp">Stamp</div>
+      <div {...stampStyle} className="stamp">Stamp</div>
     )
   }
 }
 
 export default Stamp;
+
+// .stamp {
+//
+// }
